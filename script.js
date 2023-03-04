766301e394a6a294aff18598223a257d8f079152
Filipe Vasconcelos <filipe.vasconcelos@capgemini.com>
2023-03-04 02:50:56 -0300

    Finalizando tópico de 'Conteúdos'

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const header = document.querySelector('header')
 document.addEventListener('scroll', () => {
     const position = window.pageYOffset
     
-
     if (position > 4) {
         header.style.top =
         header.style.borderTopLeftRadius =
         header.style.borderTopRightRadius = '0'
         
-        header.style.backgroundColor = '#05112bed';
+        header.style.backgroundColor = '#05112bee';
     } else {
         header.style.top =
         header.style.borderTopLeftRadius =
@@ -17,5 +16,21 @@ document.addEventListener('scroll', () => {
 
         header.style.backgroundColor = '#05112b'
     }
+})
 
+
+const cardContainer = document.querySelector('.cards')
+
+cardContainer.addEventListener('click', (event) => {
+    const card = event.composedPath()[3]
+
+    if (card.classList.contains('card')){
+        card.classList.toggle('flip')
+    }
+
+    if (card.classList.contains('flipCard')) {
+        var parent = card.parentNode
+
+        parent.classList.toggle('flip')
+    }
 })
